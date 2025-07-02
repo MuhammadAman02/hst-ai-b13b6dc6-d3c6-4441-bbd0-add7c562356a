102cdb9d2ad463321fe9a41dbb1348e6f0904b27
MuhammadAman02 <mohammada@hst.ie>
2025-07-02 14:32:00 +0500

    Codegen: app/ui/__init__.py

---
+++ app/ui/__init__.py
@@ -0,0 +1,6 @@
+"""UI package for the portfolio application."""
+
+from app.ui.pages import create_portfolio_pages
+from app.ui.components import setup_theme
+
+__all__ = ["create_portfolio_pages", "setup_theme"]
